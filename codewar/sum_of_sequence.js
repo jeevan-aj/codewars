7021bd7345664644c2b37a0dc39041d4495ad1ba
jeevan-aj <ambrahamjeevan@gmail.com>
2024-02-21 20:52:39 +0530

    something

---
+++ codewar/sum_of_sequence.js
@@ -0,0 +1,11 @@
+const sequenceSum = (begin, end, step) => {
+    if(begin>end) return 0;
+    let result = 0
+    for(let i = begin;i<=end;i+=step){
+     result += i
+    }
+  return result
+   
+};
+
+console.log(sequenceSum(2,6,2))
